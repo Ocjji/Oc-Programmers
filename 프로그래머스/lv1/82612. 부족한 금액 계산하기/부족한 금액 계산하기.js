7d103c1db060273fe65a142606d5b98047b51bf1
Ocjji <124262351+Ocjji@users.noreply.github.com>
2023-04-07 16:51:26 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -0,0 +1,16 @@
+function solution(price, money, count) {
+    let result = 0;
+    var answer = -1;
+
+    for (let i = 1; i <= count; i++) {
+        result += (price * i);
+    }
+    if (result <= money) {
+        answer = 0;
+    } else if (result > money) {
+        answer = result - money;
+    }
+
+
+    return answer;
+}
