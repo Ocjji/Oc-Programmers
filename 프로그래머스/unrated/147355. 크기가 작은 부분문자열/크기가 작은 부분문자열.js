7a41829da8bad09fe8f5297cd7fe8ee3f1550cec
Ocjji <124262351+Ocjji@users.noreply.github.com>
2023-04-07 16:51:44 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 2.73 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -0,0 +1,16 @@
+function solution(t, p) {
+    let t_len = t.length;
+    let p_len = p.length;
+
+    var answer = 0;
+
+    for (let i = 0; i < t_len - p_len + 1; i++) {
+        t.substr(i, p_len);
+        console.log(t.substr(i, p_len));
+
+        if (t.substr(i, p_len) <= p) {
+            answer++;
+        }
+    }
+    return answer;
+}
