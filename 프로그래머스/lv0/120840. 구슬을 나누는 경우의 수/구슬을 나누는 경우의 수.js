2e23469f21e386a767f330992d40869c9ee0cfc2
Ocjji <124262351+Ocjji@users.noreply.github.com>
2023-04-10 21:45:36 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.js
@@ -0,0 +1,27 @@
+function solution(balls, share) {
+    var answer = 0;
+    let res_ja = 0;
+    let res_mo = 0;
+    
+    if(share === 1) {
+        return balls;
+    }
+    if(balls === share){
+        return 1;
+    }
+    
+    res_ja = ja(balls, share);
+    res_mo = ja(share, share);
+    
+    answer = parseInt(res_ja / res_mo);
+    
+    return answer;
+}
+
+let ja = (bl, sh) => {
+    if(sh === 0){
+        return 1;
+    } else {
+        return bl * ja(bl-1, sh-1);
+    }
+}
